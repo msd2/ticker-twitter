759371549f821956a5ea42488715f7fea809a4d9
Marc Dunford <marcdunford@Marcs-MacBook-Pro.local>
2021-09-14 08:47:44 +0100

    can query tweets and upload to gcp bucket

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from scraper import TweetCollector
+from scraper import authenticate, tweet_grabber, upload_to_bucket
 from config import api_key, api_secret
-import tweepy
 
-tweets = TweetCollector(api_key, api_secret)
-data = tweets.get_tweets('#ETH','en',2000)
-data = tweets.find_sentiment(data)
-data.to_csv('tweet_data.csv', index=False)
-auth = tweepy.AppAuthHandler(api_key, api_secret)
+api = authenticate(api_key, api_secret)
+data = tweet_grabber('government_twitter_handles.txt', api, 5)
+data.to_csv('temp_data/tweets.csv', index=False)
+upload_to_bucket('tweets', 'temp_data/tweets.csv', 'uk-gov-tweets-14289')
--- scraper.py
+++ scraper.py
@@ -1,19 +1,40 @@
 import tweepy
-from config import api_key, api_secret
 import pandas as pd
 from collections import defaultdict
+from google.cloud import storage
 
 def authenticate(api_key, api_secret):
     auth = tweepy.AppAuthHandler(api_key, api_secret)
     api = tweepy.API(auth)
     return api
 
-def get_tweets(api, search, lang, items):
-    d=defaultdict(list)
-    q=search+'-filter:retweets'
-    for tweet in tweepy.Cursor(api.search, q=q, lang=lang).items(items):
-        d['tweet'].append(tweet.text)
-        d['created'].append(tweet.created_at)
-    df = pd.DataFrame(d)
-    return df
+def tweet_grabber(twitter_handles, api, number_of_tweets):
+    d = defaultdict(list)
+    with open(twitter_handles, "r") as handles:
+        for user in handles:
+            for tweet in tweepy.Cursor(api.user_timeline, screen_name=user).items(number_of_tweets):
+                d['text'].append(tweet.text)
+                d['created'].append(tweet.created_at)
+                d['user'].append(tweet.user.name)
+    tweets = pd.DataFrame(d)
+    return tweets
 
+def upload_to_bucket(blob_name, source_file_name, bucket_name):
+    """ Upload data to a bucket"""
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(
+        'twitter-325919-9d64dbc990f3.json')
+
+    #print(buckets = list(storage_client.list_buckets())
+
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, blob_name
+        )
+    )
