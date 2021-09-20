4dfd12033b389b57acc554b55e9464cdf1a611ff
Marc Dunford <marcdunford@Marcs-MacBook-Pro.local>
2021-09-20 20:53:15 +0100

    added tweet blob to gcp

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from scraper import authenticate, return_politician_handles, tweet_grabber, upload_to_bucket
+from config import api_key, api_secret
+from datetime import date
+import pandas as pd
+
+today = date.today()
+today = today.strftime("%Y_%m_%d")
+
+api = authenticate(api_key, api_secret)
+politician_twitter_handles = return_politician_handles()
+batchsize = 50
+
+frames = []
+for i in range(0, len(politician_twitter_handles), batchsize):
+    batch = politician_twitter_handles[i:i+batchsize]
+    data = tweet_grabber(batch, api, 20)
+    frames.append(data)
+
+data = pd.concat(frames)
+print(data)
+data.to_csv('temp_data/tweets.csv', index=False)
+upload_to_bucket('tweets_'+today, 'temp_data/tweets.csv', 'uk-gov-tweets-14289')
---
+++ app/scraper.py
@@ -0,0 +1,65 @@
+import tweepy
+import pandas as pd
+from collections import defaultdict
+from google.cloud import storage
+from urllib.request import Request, urlopen
+from io import StringIO
+
+
+
+def authenticate(api_key, api_secret):
+    auth = tweepy.AppAuthHandler(api_key, api_secret)
+    api = tweepy.API(auth)
+    print('Twitter authentication successful.\n')
+    return api
+
+
+
+
+def tweet_grabber(twitter_handles, api, number_of_tweets):
+    d = defaultdict(list)
+    print('Scraping tweets. Handle:')
+    for user in twitter_handles:
+        print(user)
+        try:
+            for tweet in tweepy.Cursor(api.user_timeline, screen_name=user).items(number_of_tweets):
+                d['id'].append(tweet.id)
+                d['text'].append(tweet.text)
+                d['created'].append(tweet.created_at)
+                d['user'].append(tweet.user.name)
+        except tweepy.error.TweepError:
+            print("user doesn't exist")
+            pass
+    tweets = pd.DataFrame(d)
+    print('Tweets scraped.\n')
+    return tweets
+
+
+
+
+def upload_to_bucket(blob_name, source_file_name, bucket_name):
+    print('Uploading to bucket.')
+    storage_client = storage.Client.from_service_account_json(
+        'creds.json')
+
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, blob_name
+        )
+    )
+
+
+
+def return_politician_handles():
+    req = Request('https://www.politics-social.com/api/list/csv/followers', headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    s=str(webpage,'utf-8')
+    data = StringIO(s) 
+    df=pd.read_csv(data)
+    politician_handles = df['Screen name'].apply(lambda x: x[1:])
+    print('Politician twitter handles imported.\n')
+    return politician_handles
